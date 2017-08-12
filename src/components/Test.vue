1375681e777ceae03803abcd3040ea8393d0abc9
riksameidy <riksameidy@gmail.com>
2017-08-12 20:52:28 +0700

    vue app

---
+++ src/components/Test.vue
@@ -0,0 +1,78 @@
+<template>
+
+<div class="test">
+  <h1>{{title}}</h1>
+  <input type="text" v-model="title">
+  <p v-if="showname">{{user.first_name}}</p>
+  <p v-else="showname">Nobody</p>
+  <!-- <p v-text="user.first_name"></p> -->
+  <ul>
+    <li v-for="item in items">
+      {{item.title}}
+    </li>
+  </ul>
+
+  <button v-on:click="greet('Hello World')">Click</button>
+  <br>
+  <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit" name="" value="">
+  <hr>
+  <label for="">First Name</label><input type="text" v-model="user.first_name" name="" value="">
+  <br>
+  <label for="">Last Name</label><input type="text" v-model="user.last_name" name="" value="">
+  <h3>{{fullname}}</h3>
+  <h3>{{msg}}</h3>
+
+</div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'test',
+    props: {
+      msg:{
+        type:String,
+        default:'foobar'
+      }
+    },
+    data(){
+      return{
+      title:'Hello World' ,
+      user: {
+        first_name: 'Riksa',
+        last_name: 'Meidy'
+      },
+      showname:true,
+      items: [
+        {title: 'item one'},
+        {title: 'item two'},
+        {title: 'item three'}
+      ]
+    };
+  },
+  methods: {
+    greet : function(greeting){
+      alert(greeting);
+    },
+    pressKey: function(e){
+      console.log(e.target.value);
+    },
+    enterHit: function(){
+      console.log('You Hit Enter');
+    }
+  },
+  computed:{
+    fullname: function(){
+      return this.user.first_name + ' ' + this.user.last_name;
+    }
+  }
+
+  }
+
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import vueResource from 'vue-resource'
+import Users from './components/users'
+import Test from './components/test'
+
+Vue.config.productionTip = false
+Vue.use(vueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode:'history',
+  base: __dirname,
+  routes: [
+    {path:'/', component: Users},
+    {path:'/test', component: Test}
+  ]
+});
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  template: `
+    <div id="app">
+    <ul>
+      <li> <router-link to="/">Users </router-link></li>
+      <li> <router-link to="/test">Test </router-link> </li>
+      <router-view>
+
+      </router-view>
+     </ul>
+
+     </div>
+   `
+}).$mount('#app')
